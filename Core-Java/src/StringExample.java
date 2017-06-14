e970acb78527638d7f7d29a3a9a0e331872b0930
milad-milix <milad.zahery@gmail.com>
2017-06-13 23:45:36 -0400

    string class example

--- Core-Java/src/StringExample.java
+++ Core-Java/src/StringExample.java
@@ -1,9 +1,11 @@
 
 public class StringExample {
 	
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		
 		String string = "Milad Zaheri";
+		
+		//substring method
 		System.out.println("First 5 char String: " + string.substring(0, 5));
 
 	}
