6a092ba1084b6702a2179febffd685b989bdef2b
milad-milix <milad.zahery@gmail.com>
2017-06-13 23:43:08 -0400

    string class example

---
+++ Core-Java/src/StringExample.java
@@ -0,0 +1,11 @@
+
+public class StringExample {
+	
+	public static void main(String[] args) {
+		
+		String string = "Milad Zaheri";
+		System.out.println("First 5 char String: " + string.substring(0, 5));
+
+	}
+
+}
