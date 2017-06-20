27362b63bb488682eeca46a6bf9e722c19d44a92
milad-milix <milad.zahery@gmail.com>
2017-06-19 21:52:26 -0400

    priority queue example

--- Core-Java/src/PriorityQExample.java
+++
@@ -1,8 +0,0 @@
-
-public class PriorityQExample {
-	
-	public static void main(String [] args) {
-		
-	}
-
-}
--- Core-Java/src/PriorityQueueExample.java
+++ Core-Java/src/PriorityQueueExample.java
@@ -1,12 +1,51 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class PriorityQueueExample {
 	
 	public static void main(String [] args) {
 		
+		//natural ordering example of priority queue
+		Queue<Integer> integerPriorityQueue = new PriorityQueue<>(5);
+		Random rand = new Random();
+		for(int i=0;i<5;i++){
+			integerPriorityQueue.add(new Integer(rand.nextInt(50)));
+		}
+		for(int i=0;i<5;i++){
+			Integer in = integerPriorityQueue.poll();
+			System.out.println("Processing Integer:"+in);
+		}
 		
-
-
+		//PriorityQueue example with Comparator
+		Queue<SampleClass> comparatorPriorityQueue = new PriorityQueue<>(5, idComparator);
+		addDataToQueue(comparatorPriorityQueue);
+		pollDataFromQueue(comparatorPriorityQueue);
 	}
+	
+	//Comparator anonymous class implementation
+	public static Comparator<SampleClass> idComparator =  new Comparator<SampleClass>(){
+		@Override
+		public int compare(SampleClass c1, SampleClass c2){
+			return (int) (c1.getId() - c2.getId());
+		}
+	};
+	
+	//utility method to add random data to Queue
+	private static void addDataToQueue(Queue<SampleClass> sampleClassPriorityQueue) {
+		Random rand = new Random();
+		for(int i=0;i<5;i++) {
+			int id = rand.nextInt(30);
+			sampleClassPriorityQueue.add(new SampleClass(id, "Employee "+id));
+		}
+	}
+	
+	//utility method to pull data from Queue
+	private static void pollDataFromQueue(Queue<SampleClass> sampleClassPriorityQueue) {
+		while(true) {
+			SampleClass employee = sampleClassPriorityQueue.poll();
+			if(employee == null) break;
+			System.out.println("Processing employee with ID="+employee.getId());
+		}
+	}
+		
 
 }
